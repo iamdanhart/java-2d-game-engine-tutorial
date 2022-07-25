c4a07c9835dff00177aae3b2649e51fffe5a09b9
Danny Hart <git@danielhart.me>
2022-07-25 17:37:18 -0400

    Change first window color to white

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -97,7 +97,7 @@ public class Window {
             // poll events
             glfwPollEvents();
 
-            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
 
             glfwSwapBuffers(glfwWindow);
